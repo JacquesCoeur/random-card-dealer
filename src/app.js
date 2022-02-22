ce214b17ad00e9fee368de60a08f8394d54be4bb
Diego_M <diegofernandomzlz@gmail.com>
2022-02-22 05:40:19 +0000

    random-card-dealer

--- src/app.js
+++ src/app.js
@@ -6,6 +6,42 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let type = document.querySelector("#cardWhite");
+  let valor = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let typeArr = Array.from(type.children);
+
+  let typeRandom = typeArr[Math.floor(Math.random() * 4)];
+
+  let valorRandom = valor[Math.floor(Math.random() * valor.length)];
+
+  if (typeRandom == document.querySelector(".heart")) {
+    document.querySelector(".heart").innerHTML = "♥";
+    document.querySelector(".heartFlipV").innerHTML = "♥";
+  } else if (typeRandom == document.querySelector(".diamond")) {
+    document.querySelector(".diamond").innerHTML = "♦";
+    document.querySelector(".diamondFlipV").innerHTML = "♦";
+  } else if (typeRandom == document.querySelector(".spade")) {
+    document.querySelector(".spade").innerHTML = "♠";
+    document.querySelector(".spadeFlipV").innerHTML = "♠";
+  } else if (typeRandom == document.querySelector(".club")) {
+    document.querySelector(".club").innerHTML = "♣";
+    document.querySelector(".clubFlipV").innerHTML = "♣";
+  } else {
+    console.log("REVISA TUS CONDICIONALES ALGO ANDA MAL");
+  }
+  document.querySelector(".valorRandom").innerHTML = valorRandom;
 };
